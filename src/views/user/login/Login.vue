db1619ea3b303d53054a7c820032ecb8e191198c
MigAnette <anettesommer@live.dk>
2019-05-16 09:04:26 +0200

    Login page added

---
+++ src/views/user/login/Login.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <!-- arrow button back to frontPage -->
+
+        <!-- Text saying Login -->
+        <!-- Input field for Email -->
+        <!-- Input field for password -->
+
+        <!-- button for login when input fields have been filled out -->
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        // on-click for the button has to update/check email password use GeoNinja file
+    }
+}
+</script>
+
+<style>
+
+</style>
