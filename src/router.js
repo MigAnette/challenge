629d12273de029a05577b31d35c334f437777e16
MigAnette <anettesommer@live.dk>
2019-05-16 12:27:25 +0200

    Added Home, Profile and Examples

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from '@/views/home/Home';
+import Profile from '@/views/profile/Profile';
+import Examples from '@/views/exampleChal/Examples';
 
 Vue.use(Router)
 
@@ -8,6 +10,25 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    
+    {
+      // Home
+      path: '/hjem',
+      name: 'Home',
+      component: Home
+    },
+    {
+      // Profile
+      path: '/profil/:user',
+      // has to go to user profile page
+      name: 'Profile',
+      component: Profile
+    },
+    {
+      // Examples
+      path: '/eksempler',
+      name: 'Examples',
+      component: Examples
+    },
+
   ]
 })
