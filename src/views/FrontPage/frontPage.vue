c87dedd663c4776cd9f17e18df625a5dcdffd14f
MigAnette <anettesommer@live.dk>
2019-05-16 08:50:01 +0200

    Frontpage added

---
+++ src/views/FrontPage/frontPage.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <!-- Login link going to login page -->
+
+        <!-- slider with different messages about the app -->
+        <!-- the slider needs next and previos arrows -->
+
+        <!-- Link with Se Udfordringerne her that goes to the examples -->
+
+
+        <!-- dots that show which of the slides you are at -->
+
+    </div>
+    
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        // a function for next button on the slider
+
+        // a function for previous button on the slider
+    }
+}
+</script>
+
+<style>
+
+</style>
